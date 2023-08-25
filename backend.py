ca4943ab2426d4421b73bd9b4cf8f817ffe96b81
MonishSannathi <sannathimonish@gmail.com>
2023-08-25 11:22:59 -0400

    Added Comments

--- backend.py
+++ backend.py
@@ -1,4 +1,24 @@
 import requests
+#Dummy Class for 
+class name{
+    int quant;
+    int price;
+}
+List<name> bids = new ArrayList<>();
+List<name> asks = new ArrayList<>();
+
+Gemini Data
+for data in GeminiData{
+    if(data.price ){
+        Name name = new Name();
+        name.price = data.price
+        name.qua = 
+        bids.add(name);
+    }
+}
+
+#Should Contain Quantity and Price as the Parameters
+#Dummy Class for bids
 
 def fetch_order_book(exchange_url):
     response = requests.get(exchange_url)
@@ -13,21 +33,29 @@ def accumulate_orders(orders, threshold):
     total_amount = 0.0
     for order in orders:
         amount = float(order['amount'])
-        if total_amount + amount <= threshold:
+        if total_amount + amount < threshold:
             accumulated.append(order)
             total_amount += amount
+        while #condition(total_amount + amount >= threshold):
+            #Sliding window Approach
         else:
             remaining_amount = threshold - total_amount
             fraction = remaining_amount / amount
             partial_order = order.copy()
+            #Need Quantity in the partial order
             partial_order['amount'] = str(remaining_amount)
             accumulated.append(partial_order)
+            #increment the left pointers
             break
+        #Final List should contain quantity and amount values
+        #Use Dummy Class
     return accumulated
 
 def merge_order_books(order_books):
     merged_bids = []
     merged_asks = []
+    #bids and asks contain items of dictionary
+    #each dictionary item contain keys as quantity and price
 
     for order_book in order_books:
         bids = order_book.get("bids", [])
@@ -35,6 +63,8 @@ def merge_order_books(order_books):
         
         merged_bids.extend(bids)
         merged_asks.extend(asks)
+
+        #Need quantity along with price
 
         merged_bids.sort(key=lambda x: float(x['price']), reverse=True)
         merged_asks.sort(key=lambda x: float(x['price']))
@@ -47,6 +77,16 @@ def merge_order_books(order_books):
 
     return accumulated_bids, accumulated_asks
 
+def gemini_adapt_order_books(order_book):
+    #Code to adapt the Gemini Exchange data to Locally Declared Classes
+    #Use Dummy Class
+    #for loop to adapt data
+
+def kraken_adapt_order_books(order_book):
+    #Code to adapt the Kranken Exchange data to Locally Declared Classes
+    #Use Dummy Class
+    #for loop to adapt data
+
 if __name__ == "__main__":
     kraken_order_book_url = "https://api.kraken.com/0/public/Depth?pair=XBTUSD"
     gemini_order_book_url = "https://api.gemini.com/v1/book/BTCUSD"
@@ -56,7 +96,8 @@ if __name__ == "__main__":
     
     if kraken_order_book and gemini_order_book:
         order_books = [kraken_order_book, gemini_order_book]
-        
+        #Call the adapt order books # input the exchange book and output the local data
+        #Then add the returned local ordered books
         merged_bids, merged_asks = merge_order_books(order_books)
         try:
             buy_price = float(merged_asks[0]['price'])  # Get the 'price' value from the dictionary
